81b4e58314781680e2de8b258ea6789f540c6a2f
Touched <Touched@users.noreply.github.com>
2014-09-19 19:33:51 +0200

    Created GPIO base class for sensors.

---
+++ src/atlantis/sensor/GPIO.cpp
@@ -0,0 +1,11 @@
+#include "GPIO.h"
+
+GPIO::GPIO() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPIO::~GPIO() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/atlantis/sensor/GPIO.h
@@ -0,0 +1,33 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "Sensor.h"
+
+/*
+ * Base class for implementation of sensors that utilise the 4bit GPIO port in
+ * the ROM chip. Some cartidges use this interface to implement an RTC or
+ * or game-specific sensors.
+ */
+
+class GPIO: public Sensor {
+public:
+	GPIO();
+	virtual ~GPIO();
+
+	bool isInAddressRange(u32 address);
+
+	bool isEnabled();
+
+	int write(u32 address, u32 value);
+	int write(u32 address, u16 value);
+	int write(u32 address, u8 value);
+
+	u32 read_word(u32 address);
+	u16 read_hword(u32 address);
+	u16 read_byte(u32 address);
+	int read(u32 address, u32 *out);
+	int read(u32 address, u16 *out);
+	int read(u32 address, u8 *out);
+};
+
+#endif /* GPIO_H_ */
